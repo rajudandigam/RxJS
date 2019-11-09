0803d16e773695874ba6072ffce5a0c166929778
Raju Dandigam <raju0574@gmail.com>
2019-11-09 08:53:21 -0800

    feat: added obervable and observer with subscription

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,27 @@
-import * as Rx from "rxjs";
+import { Observable } from "rxjs";
 
-console.log(Rx);
+const addItem = (item: any) => {
+  const rootElem = document.getElementById('root');
+  const liElem = document.createElement('li');
+  const itemText = document.createTextNode(item);
+
+  liElem.appendChild(itemText);
+  rootElem.appendChild(liElem);
+};
+
+const observable = Observable.create((observer: any) => {
+  try {
+    observer.next('Mission started');
+    observer.next('Mission Running 1');
+    observer.complete();
+    observer.next('This after completion');
+  } catch(err) {
+    observer.error(err);
+  }
+});
+
+observable.subscribe(
+  (item: any) => addItem(item),
+  (error: any) => addItem(error),
+  () => addItem('Mission accomplished')
+);
